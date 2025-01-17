7136150c4b46b6518fc721655c15824706c7d185
Chaitany233patil <patilchaitany529@gmail.com>
2025-01-18 00:31:16 +0530

    add accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Die from "./componets/Die";
 import { nanoid } from "nanoid";
@@ -6,14 +6,20 @@ import Confetti from "react-confetti";
 
 function App() {
   const [dice, setDice] = useState(() => getAllDiceNumber());
+  const rollBtn = useRef(null);
 
   const isWon = dice.every(
     (diceObj) =>
       diceObj.isHeld === dice[0].isHeld && diceObj.value === dice[0].value
   );
 
+  useEffect(() => {
+    if (isWon) {
+      rollBtn.current.focus();
+    }
+  }, [isWon]);
+
   function getAllDiceNumber() {
-    console.log("Hello");
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
@@ -29,15 +35,6 @@ function App() {
     );
   }
 
-  const dieElements = dice.map((diceObj) => (
-    <Die
-      hold={() => hold(diceObj.id)}
-      key={diceObj.id}
-      value={diceObj.value}
-      isHeld={diceObj.isHeld}
-    />
-  ));
-
   function rollDice() {
     if (!isWon) {
       setDice((prevDice) =>
@@ -50,6 +47,15 @@ function App() {
     }
   }
 
+  const dieElements = dice.map((diceObj) => (
+    <Die
+      hold={() => hold(diceObj.id)}
+      key={diceObj.id}
+      value={diceObj.value}
+      isHeld={diceObj.isHeld}
+    />
+  ));
+
   return (
     <main>
       <p className="title">Tenzies</p>
@@ -59,7 +65,7 @@ function App() {
       </div>
       {isWon && <Confetti />}
       <div className="container">{dieElements}</div>
-      <button onClick={rollDice} className="rollDice">
+      <button onClick={rollDice} className="rollDice" ref={rollBtn}>
         {isWon ? "Start New Game" : "Roll"}
       </button>
       <p style={{ marginTop: "10px" }}>
